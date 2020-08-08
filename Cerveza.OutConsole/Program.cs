ebbcea381a2f1460a5266b5e47b340505af6859a
xafilk <carva97valvi@gmail.com>
2020-08-08 02:13:15 -0600

    Remove OutConsole Test Code

--- Cerveza.OutConsole/Program.cs
+++ Cerveza.OutConsole/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Cerveza.DataAccessLogic.JsonAccess;
 
 namespace Cerveza.OutConsole
 {
@@ -11,8 +10,6 @@ namespace Cerveza.OutConsole
     {
         static void Main(string[] args)
         {
-            JsonDataReader json = new JsonDataReader();
-            json.GetUsers();
 
         }
     }
